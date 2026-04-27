2b883a78f4cda2fa7db0dce777a4b23999bd4c4f
andrelsrn <andrel.srn@hotmail.com>
2026-04-27 15:08:32 -0300

    initialize accident alert application with Spring Boot setup and Maven wrapper

---
+++ src/test/java/dev/andre/accidentalert/AccidentAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andre.accidentalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
